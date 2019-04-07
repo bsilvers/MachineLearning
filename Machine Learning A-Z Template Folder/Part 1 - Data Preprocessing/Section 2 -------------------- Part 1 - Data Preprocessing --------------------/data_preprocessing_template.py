54f6cb07c50766b72c65b86d4ac621ce35214359
bsilvers <bsilvers@yahoo.com>
2019-04-07 15:59:13 -0500

    initial checkin

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 15:50:52 2019
+
+@author: Brian Silvers
+"""
+# Data Preprocessing
+
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
