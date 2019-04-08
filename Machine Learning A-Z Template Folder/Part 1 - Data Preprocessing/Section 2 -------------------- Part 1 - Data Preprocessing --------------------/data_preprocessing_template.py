d1a94f265bcc4f68e2641c959c4b171c945f7686
Brian Silvers <bsilvers@yahoo.com>
2019-04-07 20:37:43 -0500

    progress

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -20,4 +20,4 @@ y = dataset.iloc[:, 3].values
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3]
+X[:, 1:3] = imputer.transform(X[:, 1:3])
